f26bb07dab898fdeaf682ba284a15f04ddd71eda
Rex Lorenzo <rex@oid.ucla.edu>
2012-07-27 16:30:48 -0700

    CONTRIB-3806 - Including idnumber in results download

--- locallib.php
+++ locallib.php
@@ -2431,7 +2431,7 @@ class questionnaire {
             $groupid = -1;
         }
         $output = array();
-        $nbinfocols = 9; // change this if you want more info columns
+        $nbinfocols = 10; // change this if you want more info columns
         $stringother = get_string('other', 'questionnaire');
         $columns = array(
                 get_string('response','questionnaire'),
@@ -2442,6 +2442,7 @@ class questionnaire {
                 get_string('group'),
                 get_string('id','questionnaire'),
                 get_string('fullname'),
+                get_string('idnumber'),
                 get_string('username')
             );
 
@@ -2453,6 +2454,7 @@ class questionnaire {
                 1,
                 1,
                 0,
+                1,
                 1,
                 1,
             );
@@ -2701,6 +2703,7 @@ class questionnaire {
                     $uid = $username;
                     $fullname = fullname($user);
                     $username = $user->username;
+                    $uidnumber = $user->idnumber;
                 }
             }
 
@@ -2727,6 +2730,7 @@ class questionnaire {
                 $fullname =  get_string('anonymous', 'questionnaire');
                 $username = '';
                 $uid = '';
+                $uidnumber = '';
             }
             $arr = array();
             array_push($arr, $qid);
@@ -2737,6 +2741,7 @@ class questionnaire {
             array_push($arr, $groupname);
             array_push($arr, $uid);
             array_push($arr, $fullname);
+            array_push($arr, $uidnumber);
             array_push($arr, $username);
 
             // merge it
